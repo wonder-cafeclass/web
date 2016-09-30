103a75be6bb56716162ab83fa1e6ed346cd9278c
wonder13662 <wonder13662@gmail.com>
2016-09-30 12:05:50 +0900

    sql updated
    1. basic table create query added
    - user (kakao/facebook/google)
    - access_log
    - error_log
    - action_log

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -25,9 +25,7 @@ class Users extends REST_Controller {
 
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
-        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+        $this->methods['list_get']['limit'] = 500; // 500 requests per hour per user/key
     }
 
     public function list_get()
@@ -62,6 +60,8 @@ class Users extends REST_Controller {
             $this->set_response($response_body, REST_Controller::HTTP_NOT_FOUND); 
         }
     }
+    
+    // REMOVE ME
 /*
     public function users_post()
     {
