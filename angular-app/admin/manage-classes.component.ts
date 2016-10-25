d05bc549f7624cfb27d54bbc296b622039487591
wonder13662 <wonder13662@gmail.com>
2016-10-25 11:12:23 +0900

    remove unused angular app
    1. no desc

--- angular-app/admin/manage-classes.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  template:  `
-    <p>Manage your classes here</p>
-  `
-})
-export class ManageClassesComponent { }
--- angular-app/class-center/class.ts
+++
@@ -1,4 +0,0 @@
-export class Class {
-	id: number;
-	name: string;
-}
